576c20f332ce7470a276772f7838224f5f8049d2
luvu2 <spdlqj9777@likelion.org>
2022-01-20 09:40:45 +0900

    Update wall3d.js

--- Interactive_section11-3d-wall/wall3d.js
+++ Interactive_section11-3d-wall/wall3d.js
@@ -1,9 +1,18 @@
 (function () {
 
     const houseElem = document.querySelector('.house');
+    let maxScrollValue;
+
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
 
     window.addEventListener('scroll', function () {
-        console.log(pageYOffset);
+        const zMove = pageYOffset / maxScrollValue * 980 - 490;
+        houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
     });
+
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
 
 })();
