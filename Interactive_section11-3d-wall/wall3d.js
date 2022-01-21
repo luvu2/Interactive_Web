e1b4184ed78caa2b9b66ae3f8cd976add14f65b5
luvu2 <spdlqj9777@likelion.org>
2022-01-21 22:22:28 +0900

    Update files

--- Interactive_section11-3d-wall/wall3d.js
+++ Interactive_section11-3d-wall/wall3d.js
@@ -1,6 +1,8 @@
 (function () {
-
+    const stageElem = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar');
+    const mousePos = { x: 0, y: 0 };
     let maxScrollValue;
 
     function resizeHandler() {
@@ -8,8 +10,19 @@
     }
 
     window.addEventListener('scroll', function () {
-        const zMove = pageYOffset / maxScrollValue * 980 - 490;
+        const scrollPer = pageYOffset / maxScrollValue;
+        const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+        //progress bar
+        barElem.style.width = scrollPer * 100 + '%';
+    });
+
+    window.addEventListener('mousemove', function (e) {
+        // mouse canvas
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)'; 
     });
 
     window.addEventListener('resize', resizeHandler);
