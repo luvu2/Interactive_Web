0628a760680bf781aa3790735c484ac60ae18b3d
luvu2 <spdlqj9777@likelion.org>
2022-01-27 23:34:55 +0900

    Create Character.js

---
+++ Interactive_section11-3d-wall/Character.js
@@ -0,0 +1,31 @@
+function Character() {
+    this.mainELem = document.createElement('div');
+    this.mainELem.classList.add('character');
+    this.mainELem.innerHTML = ''
+        + '<div class="character-face-con character-head">'
+           +  '<div class="character-face character-head-face face-front"></div>'
+           +  '<div class="character-face character-head-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-torso">'
+            + '<div class="character-face character-torso-face face-front"></div>'
+            + '<div class="character-face character-torso-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-arm character-arm-right">'
+            + '<div class="character-face character-arm-face face-front"></div>'
+            + '<div class="character-face character-arm-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-arm character-arm-left">'
+            + '<div class="character-face character-arm-face face-front"></div>'
+            + '<div class="character-face character-arm-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-leg character-leg-right">'
+            + '<div class="character-face character-leg-face face-front"></div>'
+            + '<div class="character-face character-leg-face face-back"></div>'
+        + '</div>'
+        + '<div class="character-face-con character-leg character-leg-left">'
+            + '<div class="character-face character-leg-face face-front"></div>'
+            + '<div class="character-face character-leg-face face-back"></div>'
+        + '</div>';
+
+    document.querySelector('.stage').appendChild(this.mainELem);
+}
