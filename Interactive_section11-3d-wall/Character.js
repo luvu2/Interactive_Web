dbeb27b3ee2555a506d4b421c3858d3dfbab7ce0
luvu2 <spdlqj9777@likelion.org>
2022-01-31 20:22:07 +0900

    Update files

--- Interactive_section11-3d-wall/Character.js
+++ Interactive_section11-3d-wall/Character.js
@@ -1,7 +1,7 @@
 function Character(info) {
-    this.mainELem = document.createElement('div');
-    this.mainELem.classList.add('character');
-    this.mainELem.innerHTML = ''
+    this.mainElem = document.createElement('div');
+    this.mainElem.classList.add('character');
+    this.mainElem.innerHTML = ''
     + '<div class="character-face-con character-head">'
     + '<div class="character-face character-head-face face-front"></div>'
     + '<div class="character-face character-head-face face-back"></div>'
@@ -27,11 +27,15 @@ function Character(info) {
     + '<div class="character-face character-leg-face face-back"></div>'
 + '</div>';
 
-    document.querySelector('.stage').appendChild(this.mainELem);
+    document.querySelector('.stage').appendChild(this.mainElem);
 
-    this.mainELem.style.left = info.xPos + '%';
+    this.mainElem.style.left = info.xPos + '%';
     // 스크롤 중인지 아닌지
     this.scrollState = false;
+    // 바로 이전(마지막) 스크롤 위치
+    this.lastScrollTop = 0;
+    this.xPos = info.xPos;
+    this.speed = 1;
     this.init();
 }
 
@@ -44,13 +48,45 @@ Character.prototype = {
             clearTimeout(self.scrollState);
 
             if (!self.scrollState) {
-                self.mainELem.classList.add('running');
+                self.mainElem.classList.add('running');
             }
 
             self.scrollState = setTimeout(function () {
                 self.scrollState = false;
-                self.mainELem.classList.remove('running');
+                self.mainElem.classList.remove('running');
             }, 500);
+
+            // 이전 스크롤 위치와 현재 스크롤 위치를 비교
+            if (self.lastScrollTop > pageYOffset) {
+                // 이전 스크롤 위치가 크다면: 스크롤 올림
+                self.mainElem.setAttribute('data-direction', 'backward');
+            } else {
+                // 현재 스크롤 위치가 크다면: 스크롤 내림
+                self.mainElem.setAttribute('data-direction', 'forward');
+            }
+
+            self.lastScrollTop = pageYOffset;
+        });
+
+        window.addEventListener('keydown', function (e) {
+            console.log('keydown');
+            if (e.keyCode == 37) {
+                // 왼쪽
+                self.direction = 'left';
+                self.mainElem.setAttribute('data-direction', 'left');
+                self.mainElem.classList.add('running');
+                self.xPos -= self.speed;
+                self.mainElem.style.left = self.xPos + '%';
+            } else if (e.keyCode == 39) {
+                // 오른쪽
+                self.direction = 'right';
+                self.mainElem.setAttribute('data-direction', 'right');
+                self.mainElem.classList.add('running');
+            }
+        });
+
+        window.addEventListener('keyup', function (e) {
+            self.mainElem.classList.remove('running');
         });
     }
 };
